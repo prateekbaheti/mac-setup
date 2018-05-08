9dfa349f7da2411b7385f15eea5e0373fc23313b
Prateek Baheti <prateektheone@gmail.com>
2018-05-08 16:01:59 +0530

    Adding install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo "Installing Homebrew"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew tap caskroom/cask
+
+echo "Installing ansible"
+brew install ansible
+
+echo "Installing required roles for ansible"
+ansible-galaxy install -r requirements.yml
+
+echo "Installing ohmyzsh"
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo "Installing apps"
+ansible-playbook main.yml
