f9c5150ccd860fb57c373a65e1de3dfa6d5a041f
Prateek Baheti <prateekbaheti@Prateeks-MacBook-Pro.local>
2019-08-01 23:13:35 +0800

    Adding --ask-become-pass arg to ansible command

--- install.sh
+++ install.sh
@@ -16,4 +16,4 @@ echo "Installing ohmyzsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 echo "Installing apps"
-ansible-playbook main.yml
+ansible-playbook main.yml --ask-become-pass
