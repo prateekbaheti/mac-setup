5754dd97004aa49a3426462c8854c8c2f7b58745
Prateek Baheti <prateek.baheti@moneysmart.com>
2021-09-02 14:24:05 +0530

    Add dotfiles

--- install.sh
+++ install.sh
@@ -2,15 +2,15 @@
 
 xcode-select --install
 
-echo "Installing ansible"
-brew install ansible
+export PATH="$HOME/Library/Python/3.8/bin:/opt/homebrew/bin:$PATH"
+sudo pip3 install --upgrade pip
+pip3 install ansible
 
 echo "Installing required roles for ansible"
 ansible-galaxy install -r requirements.yml
 
-echo "Installing ohmyzsh"
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 echo "Installing apps"
 ansible-playbook main.yml --ask-become-pass
+
+echo "Installing ohmyzsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
