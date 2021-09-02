53852a5dea0a67318c361b4591db21ff6defcd8c
Prateek Baheti <prateek.baheti@moneysmart.com>
2021-09-02 08:46:21 +0530

    updates for new version

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Installing Homebrew"
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-brew tap caskroom/cask
+xcode-select --install
 
 echo "Installing ansible"
 brew install ansible
